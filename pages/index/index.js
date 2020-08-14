8937d3bb1dc4ec5e0b75f10de62488303b26f627
libinbin <libinbin@wondersgroup.com>
2020-08-14 17:43:31 +0800

    add more page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,63 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const date = new Date();
+const years = [];
+const months = [];
+const days = [];
+const hours = [];
+const minutes = [];
+const this_year = date.getFullYear+0;
+const this_month = date.getMonth+1;
+const vm = this;
+
+//获取年
+for (let i = 2018; i <= date.getFullYear() + 5; i++) {
+  years.push("" + i);
+}
+//获取月份
+for (let i = 1; i <= 12; i++) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  months.push("" + i);
+}
+//获取日期
+for (let i = 1; i <= 31; i++) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  days.push("" + i);
+}
+//获取小时
+for (let i = 0; i < 24; i++) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  hours.push("" + i);
+}
+//获取分钟
+for (let i = 0; i < 60; i++) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  minutes.push("" + i);
+}
 
 Page({
+  mixins: [require('../../assets/mixin/themeChanged')],
   data: {
-    motto: 'Hello World',
+    nowTime: new Date(), // 当前时间  
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    time: '',
+    // multiArray: [years, months, days, hours, minutes],
+    multiArray: [years, months],
+    multiIndex: [0, 9, 16, 10, 17],
+    choose_year: '',
+    years: new Date().getFullYear + 0,
+    months: new Date().getMonth + 1,
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,16 +66,24 @@ Page({
     })
   },
   onLoad: function () {
+    var that=this;
+    var myDate=new Date();//到当前时间的时间戳
+    //设置默认的年份
+    that.setData({
+      choose_year: that.data.multiArray[0][0],
+      years: '2020',
+      months: '08'
+    })
     if (app.globalData.userInfo) {
-      this.setData({
+      that.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (that.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
-        this.setData({
+        that.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
@@ -35,7 +93,7 @@ Page({
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
-          this.setData({
+          that.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
@@ -43,17 +101,103 @@ Page({
       })
     }
   },
+  //获取时间日期
+  bindMultiPickerChange: function(e) {
+    var that=this;
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    that.setData({
+      multiIndex: e.detail.value
+    })
+    const index = that.data.multiIndex;
+    const year = that.data.multiArray[0][index[0]];
+    const month = that.data.multiArray[1][index[1]];
+    // const day = this.data.multiArray[2][index[2]];
+    // const hour = this.data.multiArray[3][index[3]];
+    // const minute = this.data.multiArray[4][index[4]];
+    // console.log(`${year}-${month}-${day}-${hour}-${minute}`);
+    that.setData({
+      chose_date: year + '-' + month,
+      months: month,
+      years: year
+    })
+    console.log(that.data.chose_date);
+  },
+  //监听picker的滚动事件
+  bindMultiPickerColumnChange: function(e) {
+    var that=this;
+    //获取年份
+    if (e.detail.column == 0) {
+      let choose_year = that.data.multiArray[e.detail.column][e.detail.value];
+      console.log(choose_year);
+      that.setData({
+        choose_year
+      })
+    }
+    //console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+    if (e.detail.column == 1) {
+      let num = parseInt(that.data.multiArray[e.detail.column][e.detail.value]);
+      let temp = [];
+      if (num == 1 || num == 3 || num == 5 || num == 7 || num == 8 || num == 10 || num == 12) { //判断31天的月份
+        for (let i = 1; i <= 31; i++) {
+          if (i < 10) {
+            i = "0" + i;
+          }
+          temp.push("" + i);
+        }
+        that.setData({
+          ['multiArray[2]']: temp
+        });
+      } else if (num == 4 || num == 6 || num == 9 || num == 11) { //判断30天的月份
+        for (let i = 1; i <= 30; i++) {
+          if (i < 10) {
+            i = "0" + i;
+          }
+          temp.push("" + i);
+        }
+        that.setData({
+          ['multiArray[2]']: temp
+        });
+      } else if (num == 2) { //判断2月份天数
+        let year = parseInt(that.data.choose_year);
+        console.log(year);
+        if (((year % 400 == 0) || (year % 100 != 0)) && (year % 4 == 0)) {
+          for (let i = 1; i <= 29; i++) {
+            if (i < 10) {
+              i = "0" + i;
+            }
+            temp.push("" + i);
+          }
+          that.setData({
+            ['multiArray[2]']: temp
+          });
+        } else {
+          for (let i = 1; i <= 28; i++) {
+            if (i < 10) {
+              i = "0" + i;
+            }
+            temp.push("" + i);
+          }
+          that.setData({
+            ['multiArray[2]']: temp
+          });
+        }
+      }
+      console.log(that.data.multiArray[2]);
+    }
+    var data = {
+      multiArray: that.data.multiArray,
+      multiIndex: that.data.multiIndex
+    };
+    data.multiIndex[e.detail.column] = e.detail.value;
+    that.setData(data);
+  },
   getUserInfo: function(e) {
+    var that=this;
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
-    this.setData({
+    that.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  },
-  getPhoneNumber (e) {
-    console.log(e.detail.errMsg)
-    console.log(e.detail.iv)
-    console.log(e.detail.encryptedData)
   }
 })
